738c871b0f6859a05004de1e2461ee656cd20aae
MarioArango15 <mario.arango.dali.al@iespoligonosur.org>
2025-03-10 13:33:33 +0100

    Práctica concesionario

---
+++ index.php.php
@@ -0,0 +1,19 @@
+<?php include "config.php"; ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Concesionaria de Autos</title>
+</head>
+<body>
+    <h1>Gestión de Concesionaria de Autos</h1>
+    <nav>
+        <a href="clientes.php">Gestión de Clientes</a> |
+        <a href="autos.php">Gestión de Autos</a> |
+        <a href="compras.php">Registro de Compras</a> |
+        <a href="listar_compras.php">Listado de Compras</a>
+    </nav>
+</body>
+</html>
+
